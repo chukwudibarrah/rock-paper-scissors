5ba0d7e9b8009bcdad11fff1cb6fc3e76ead710c
chukwudibarrah <ceebrrh@gmail.com>
2023-01-06 18:05:50 +0000

    Added a loop option for the entire game

--- game.js
+++ game.js
@@ -10,6 +10,7 @@ let wins = 0;
 let loses = 0;
 let ties = 0;
 
+do {
 // establish game rules
 let userInput = prompt("Enter r (for rock), p (for paper) or s (for scissors):"); // create user input
 alert("You entered: " + userInput);
@@ -18,20 +19,18 @@ alert("The computer entered: " + randomChoice)
 if ((userInput === "r" && randomChoice === "s") || (userInput === "s" && randomChoice === "p") || (userInput === "p" && randomChoice === "r")) {
       wins++
       alert("You won! :)")
-      alert("Current scores - Wins:", wins++)
+      alert(wins++)
 } else if ((userInput === "r" && randomChoice === "p") || (userInput === "s" && randomChoice === "r") || (userInput === "p" && randomChoice === "s")) {
   loses++
   alert("You lost! :(")
-  alert("Current scores - Loses: " + loses++)
+  alert(loses++)
 } else if (userInput === randomChoice) {
   ties++
   alert("You tied! :|")
-  alert("Current scores - Ties: " + ties++)
+  alert(ties++)
 } else {
   alert("Your selection is not allowed. Please try again.")
 }
 
-let totalScores = [wins, loses, ties]
-for (let i = 0; i < 11; i++) {
-    alert("Total scores:", "Wins:", wins, "Loses:", loses, "Ties:", ties)  
-}
+} while (wins < 11);
+alert("Final scores:")
